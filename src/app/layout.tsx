2788875c578591a83eca487f1443dfa89bc6c063
DevMatheusGuimaraes <matheusguimaraesestudos@gmail.com>
2026-05-18 14:26:36 -0400

    ajuste meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
               t.src=v;s=b.getElementsByTagName(e)[0];
               s.parentNode.insertBefore(t,s)}(window, document,'script',
               'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1514828383417762');
+              fbq('init', '1514298786758362');
               fbq('track', 'PageView');
             `,
           }}
@@ -42,7 +42,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1514828383417762&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1514298786758362&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
